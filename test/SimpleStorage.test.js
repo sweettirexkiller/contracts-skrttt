124cf25b40960af41f21b7cbe64ab6c13a3ea52b
p.jankiewicz1 <p.jankiewicz@millionmonkeyssoftware.com>
2022-04-18 13:07:10 +0200

    what?

--- test/SimpleStorage.test.js
+++ test/SimpleStorage.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const {interface, bytecode} = require('../compile');
+const {interface: contrInterface, bytecode} = require('../compile');
 
 
 let accounts;
@@ -13,7 +13,7 @@ beforeEach(async ()=>{
     //use one of these accounts to deploy the contact
     accounts = await web3.eth.getAccounts();
 
-    storage = await new web3.eth.Contract(JSON.parse(interface))
+    storage = await new web3.eth.Contract(JSON.parse(contrInterface))
         .deploy({data: bytecode, arguments: [123]})
         .send({gas: 1000000,from: accounts[0]});
 });
